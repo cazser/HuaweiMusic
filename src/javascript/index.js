c28f4a776d250513ee6edf96f0281570fa521d1d
cazser <2264712072@qq.com>
2021-06-09 10:15:47 +0800

    可以播放音乐了

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,41 @@
 import './icons.js';
 import $ from './jquery-3.6.0.js'
 
-console.log($);
+//console.log($);
+//const $ = document.querySelector
 class Player{
     constructor(node){
-        this.root = $(node)
-    
+        this.root =typeof node === 'string'? $(node) : node;
+        this.songList =[]
+        this.currentIndex= 0;
+        this.start()
+        this.audio = new Audio();
+        this.bind() 
+    }
+
+    start(){
+        fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json')
+        .then(res=> res.json())
+        .then(data=>{
+            console.log(data)
+            this.songList = data
+        })
+    }
+
+    bind(){
+        $(".btn-play-pause", this.root).click(
+            ()=>{
+                console.log("点击了")
+                console.log(this)
+                this.playSong()
+                console.log(this.audio.src)
+            }
+        )
+   }
+
+    playSong(){
+        this.audio.src = this.songList[this.currentIndex].url
+        this.audio.play();
     }
 }
 
